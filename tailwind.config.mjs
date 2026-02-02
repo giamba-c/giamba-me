6f182d192ed3dfcbb21751ef1eb159a4e05df3d6
Giambattista Carmignani <giamba@airdigibattista.lan>
2026-02-02 17:29:59 +0100

    V1: Struttura base funzionante, Tailwind configurato e Bio inserita

---
+++ tailwind.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+            fontFamily: {
+                sans: ['Inter', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+            },
+            animation: {
+                blob: "blob 7s infinite",
+            },
+            keyframes: {
+                blob: {
+                    "0%": { transform: "translate(0px, 0px) scale(1)" },
+                    "33%": { transform: "translate(30px, -50px) scale(1.1)" },
+                    "66%": { transform: "translate(-20px, 20px) scale(0.9)" },
+                    "100%": { transform: "translate(0px, 0px) scale(1)" },
+                },
+            },
+        },
+	},
+	plugins: [],
+}
